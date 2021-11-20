b5121f908d6cf71f3e1482b8d848d38c2ba06b68
Lucky13forever <emanuellugojan@gmail.com>
2021-11-20 17:43:48 +0200

    Creating the modules

--- Design/workstation.py
+++ Design/workstation.py
@@ -1,22 +1,22 @@
-from Implementare import User
+
 
 # table_user = get_table_user()
-print(User.create_new_user('Emanuel', 'emanuel@gamil.com', '1234', 'Timis', 'Timisoara', '0724037007', 300, 2))
+# print(User.create_new_user('Emanuel', 'emanuel@gamil.com', '1234', 'Timis', 'Timisoara', '0724037007', 300, 2))
 
-table_user = User.get_table_user()
-print(table_user)
-# table_user = get_table_user()
+# table_user = User.get_table_user()
 # print(table_user)
+# # table_user = get_table_user()
+# # print(table_user)
 
-User.change_attr('1', {
-    'name' : 'Emanuel_1',
-    'email' : 'emanuel_1@gmail.com',
-    'password' : '1234_parola_1',
-    'county' : 'Arad',
-    'city' : 'Arad',
-    'phone' : '1234567890',
-    'avg_consumption' : 400,
-    'surface' : 20,
+# User.change_attr('1', {
+#     'name' : 'Emanuel_1',
+#     'email' : 'emanuel_1@gmail.com',
+#     'password' : '1234_parola_1',
+#     'county' : 'Arad',
+#     'city' : 'Arad',
+#     'phone' : '1234567890',
+#     'avg_consumption' : 400,
+#     'surface' : 20,
 
-})
+# })
 
