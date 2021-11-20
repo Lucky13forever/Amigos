15e021298442dbc0ad9acceaf22dee9cb9e6b451
Lucky13forever <emanuellugojan@gmail.com>
2021-11-20 17:36:32 +0200

    Created classs User and it's functions

--- Design/workstation.py
+++ Design/workstation.py
@@ -1,14 +1,14 @@
-from Implementare.user_functions import *
+from Implementare import User
 
 # table_user = get_table_user()
-create_new_user('Emanuel', 'emanuel@gamil.com', '1234', 'Timis', 'Timisoara', '0724037007', 300, 2)
+print(User.create_new_user('Emanuel', 'emanuel@gamil.com', '1234', 'Timis', 'Timisoara', '0724037007', 300, 2))
 
-table_user = get_table_user()
+table_user = User.get_table_user()
 print(table_user)
 # table_user = get_table_user()
 # print(table_user)
 
-change_attr('1', {
+User.change_attr('1', {
     'name' : 'Emanuel_1',
     'email' : 'emanuel_1@gmail.com',
     'password' : '1234_parola_1',
--- Implementare/user_functions.py
+++ Implementare/user_functions.py
@@ -36,101 +36,101 @@ class User():
         return len(phone) == 10
 
 
-def create_new_user(name, email, password, county, city, phone, avg_consumption, surface):
-    new_user = User(name, email, password, county, city, phone, avg_consumption, surface)
-    
-    
+    def create_new_user(name, email, password, county, city, phone, avg_consumption, surface):
+        new_user = User(name, email, password, county, city, phone, avg_consumption, surface)
+        
+        
 
-    # append the new_user
-    with open(r'Baze_de_Date\tabel_users.json', 'r') as file:
-        current = json.load(file)
-
-
-        # name should be unique
-        if User.unique_name(current, name) == False:
-            return 'Your username is taken'
-
-        # email shoulb be unique
-        if User.unique_email(current, email) == False:
-            return 'There is already a user registered with this email'
-
-        # password should have atleast a length of 4
-        if User.good_password(current, password) == False:
-            return 'Password should have atleast a length of 4'
+        # append the new_user
+        with open(r'Baze_de_Date\tabel_users.json', 'r') as file:
+            current = json.load(file)
 
 
-        # wrong county
+            # name should be unique
+            if User.unique_name(current, name) == False:
+                return 'Your username is taken'
+
+            # email shoulb be unique
+            if User.unique_email(current, email) == False:
+                return 'There is already a user registered with this email'
+
+            # password should have atleast a length of 4
+            if User.good_password(current, password) == False:
+                return 'Password should have atleast a length of 4'
 
 
-        # wrong city
-
-        # phone should have 10 digits
-        if User.good_phone(current, phone) == False:
-            return 'Phone number should have 10 digits'
-
-        k = current['0']
-
-        current[f'{k+1}'] = {
-            'name' : name,
-            'email' : email,
-            'password' : password,
-            'county' : county,
-            'city' : city,
-            'phone' : phone,
-            'avg_consumption' : avg_consumption,
-            'surface' : surface
-        }
-
-        current['0'] += 1
-
-        with open(r'Baze_de_Date\tabel_users.json', 'w') as file:
-            json.dump(current, file, indent=None)
-
-    return 'User has been added succesfully'
-
-# change indent from None to 4 to pretty print
-def change_attr(user_id, my_dict):
-    with open(r'Baze_de_Date\tabel_users.json', 'r') as file:
-        current = json.load(file)
-
-        for key, value in my_dict.items():
-            current[user_id][key] = value
-
-        with open(r'Baze_de_Date\tabel_users.json', 'w') as file:
-            json.dump(current, file, indent=None)
-
-change_attr.__doc__ = "Attributes of a user are 'name', 'email', 'password', 'county', 'city', 'phone', 'avg_consumption', 'surface'"
-
-def delete_user(user_id):
-    with open(r'Baze_de_Date\tabel_users.json', 'r') as file:
-        current = json.load(file)
-
-        del current[user_id]
-
-        with open(r'Baze_de_Date\tabel_users.json', 'w') as file:
-            json.dump(current, file, indent=4)
-
-def delete_database():
-    with open(r'Baze_de_Date\tabel_users.json', 'r') as file:
-        current = {
-            "0" : 0,
-        }
-
-        with open(r'Baze_de_Date\tabel_users.json', 'w') as file:
-            json.dump(current, file, indent=4)
+            # wrong county
 
 
+            # wrong city
 
-def get_table_user():
-    rez = {}
-    with open(r'Baze_de_Date\tabel_users.json', 'r') as file:
-        rez = json.load(file)
+            # phone should have 10 digits
+            if User.good_phone(current, phone) == False:
+                return 'Phone number should have 10 digits'
 
-        with open(r'Baze_de_Date\tabel_users.json', 'w') as file:
-            json.dump(rez, file, indent=None)
-    return rez
+            k = current['0']
 
-get_table_user.__doc__ = 'Returns the user table'
+            current[f'{k+1}'] = {
+                'name' : name,
+                'email' : email,
+                'password' : password,
+                'county' : county,
+                'city' : city,
+                'phone' : phone,
+                'avg_consumption' : avg_consumption,
+                'surface' : surface
+            }
+
+            current['0'] += 1
+
+            with open(r'Baze_de_Date\tabel_users.json', 'w') as file:
+                json.dump(current, file, indent=None)
+
+        return 'User has been added succesfully'
+
+    # change indent from None to 4 to pretty print
+    def change_attr(user_id, my_dict):
+        with open(r'Baze_de_Date\tabel_users.json', 'r') as file:
+            current = json.load(file)
+
+            for key, value in my_dict.items():
+                current[user_id][key] = value
+
+            with open(r'Baze_de_Date\tabel_users.json', 'w') as file:
+                json.dump(current, file, indent=None)
+
+    change_attr.__doc__ = "Attributes of a user are 'name', 'email', 'password', 'county', 'city', 'phone', 'avg_consumption', 'surface'"
+
+    def delete_user(user_id):
+        with open(r'Baze_de_Date\tabel_users.json', 'r') as file:
+            current = json.load(file)
+
+            del current[user_id]
+
+            with open(r'Baze_de_Date\tabel_users.json', 'w') as file:
+                json.dump(current, file, indent=4)
+
+    def delete_database():
+        with open(r'Baze_de_Date\tabel_users.json', 'r') as file:
+            current = {
+                "0" : 0,
+            }
+
+            with open(r'Baze_de_Date\tabel_users.json', 'w') as file:
+                json.dump(current, file, indent=4)
+
+
+
+    def get_table_user():
+        rez = {}
+        with open(r'Baze_de_Date\tabel_users.json', 'r') as file:
+            rez = json.load(file)
+
+            with open(r'Baze_de_Date\tabel_users.json', 'w') as file:
+                json.dump(rez, file, indent=None)
+        return rez
+
+    get_table_user.__doc__ = 'Returns the user table'
 
 # change_attr('3', {'name' : 'Andrei C.', 'parola' : 'restaurant'})
 
