18399776fc4e71a362369a217eae9542716d708a
Lucky13forever <93001393+Lucky13forever@users.noreply.github.com>
2021-11-20 17:44:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from Implementare.user_functions import User
 
 # User.create_new_user('Emanuel', 'emanuel@gmail.com', '1234', 'Timis', 'Timisoara', '0724037007', 300, 20)
 
-User.change_attr('1', {'name' : 'Emanuel is fire'})
+User.change_attr('1', {'name' : 'Andrei'})
