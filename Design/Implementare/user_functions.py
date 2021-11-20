a6c249916586a13c0d2bf861aeb20423c48e6f16
Lucky13forever <emanuellugojan@gmail.com>
2021-11-20 18:00:53 +0200

    Works

--- Design/Implementare/user_functions.py
+++ Design/Implementare/user_functions.py
@@ -42,7 +42,7 @@ class User():
         
 
         # append the new_user
-        with open(r'Baze_de_Date\tabel_users.json', 'r') as file:
+        with open(r'Design\Implementare\Baze_de_Date\tabel_users.json', 'r') as file:
             current = json.load(file)
 
 
@@ -83,50 +83,50 @@ class User():
 
             current['0'] += 1
 
-            with open(r'Baze_de_Date\tabel_users.json', 'w') as file:
+            with open(r'Design\Implementare\Baze_de_Date\tabel_users.json', 'w') as file:
                 json.dump(current, file, indent=None)
 
         return 'User has been added succesfully'
 
     # change indent from None to 4 to pretty print
     def change_attr(user_id, my_dict):
-        with open(r'Baze_de_Date\tabel_users.json', 'r') as file:
+        with open(r'Design\Implementare\Baze_de_Date\tabel_users.json', 'r') as file:
             current = json.load(file)
 
             for key, value in my_dict.items():
                 current[user_id][key] = value
 
-            with open(r'Baze_de_Date\tabel_users.json', 'w') as file:
+            with open(r'Design\Implementare\Baze_de_Date\tabel_users.json', 'w') as file:
                 json.dump(current, file, indent=None)
 
     change_attr.__doc__ = "Attributes of a user are 'name', 'email', 'password', 'county', 'city', 'phone', 'avg_consumption', 'surface'"
 
     def delete_user(user_id):
-        with open(r'Baze_de_Date\tabel_users.json', 'r') as file:
+        with open(r'Design\Implementare\Baze_de_Date\tabel_users.json', 'r') as file:
             current = json.load(file)
 
             del current[user_id]
 
-            with open(r'Baze_de_Date\tabel_users.json', 'w') as file:
+            with open(r'Design\Implementare\Baze_de_Date\tabel_users.json', 'w') as file:
                 json.dump(current, file, indent=4)
 
     def delete_database():
-        with open(r'Baze_de_Date\tabel_users.json', 'r') as file:
+        with open(r'Design\Implementare\Baze_de_Date\tabel_users.json', 'r') as file:
             current = {
                 "0" : 0,
             }
 
-            with open(r'Baze_de_Date\tabel_users.json', 'w') as file:
+            with open(r'Design\Implementare\Baze_de_Date\tabel_users.json', 'w') as file:
                 json.dump(current, file, indent=4)
 
 
 
     def get_table_user():
         rez = {}
-        with open(r'Baze_de_Date\tabel_users.json', 'r') as file:
+        with open(r'Design\Implementare\Baze_de_Date\tabel_users.json', 'r') as file:
             rez = json.load(file)
 
-            with open(r'Baze_de_Date\tabel_users.json', 'w') as file:
+            with open(r'Design\Implementare\Baze_de_Date\tabel_users.json', 'w') as file:
                 json.dump(rez, file, indent=None)
         return rez
 
@@ -138,7 +138,7 @@ class User():
 
 # delete_database()
 
-# with open(r'Baze_de_Date\tabel_users.json', 'r') as file:
+# with open(r'Design\Implementare\Baze_de_Date\tabel_users.json', 'r') as file:
 #         current = json.load(file)
 #         print(current['850'])
 
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from Implementare.user_functions import User
+from Design.Implementare.user_functions import User
 
 # User.create_new_user('Emanuel', 'emanuel@gmail.com', '1234', 'Timis', 'Timisoara', '0724037007', 300, 20)
 
-User.change_attr('1', {'name' : 'Andrei'})
+User.change_attr('1', {'name' : 'Boss'})
