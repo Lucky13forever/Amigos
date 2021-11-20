e73a434ff6e572144d6637b8d8faebb2b5da4543
Lucky13forever <emanuellugojan@gmail.com>
2021-11-20 17:23:25 +0200

    Update workstation.py

--- Design/workstation.py
+++ Design/workstation.py
@@ -1,4 +1,4 @@
-from user_functions import *
+from Implementare.user_functions import *
 
 # table_user = get_table_user()
 create_new_user('Emanuel', 'emanuel@gamil.com', '1234', 'Timis', 'Timisoara', '0724037007', 300, 2)
