6cd1093ef586096fed1734ba17230f4171ae47c1
Lucky13forever <emanuellugojan@gmail.com>
2021-11-20 19:10:37 +0200

    Change

--- Design/workstation.py
+++ Design/workstation.py
@@ -1,7 +1,16 @@
 from Implementare.user_functions import User
 
-User.delete_database()
+# User.delete_database()
+rez = User.create_new_user('Razvan_05', 'razvan@gmail.com', '1234', 'Timis', 'Timisoara', '1234567890', 400, 50)
 
+print(rez)
+
+unu = User.create_new_user('Razvan_06', 'razvan12@gmail.com', '1234', 'Timis', 'Timisoara', '123450', 400, 50)
+print(unu)
+# rez = User.get_table_user()
+# print(rez['1'])
+
+# User.change_attr('1', {'name' : 'Razvan_05'})
 # table_user = get_table_user()
 # print(User.create_new_user('Emanuel', 'emanuel@gamil.com', '1234', 'Timis', 'Timisoara', '0724037007', 300, 2))
 
