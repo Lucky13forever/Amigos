7ef077a07d31997fdad41b3c39f301e9e4c3baa9
Lucky13forever <emanuellugojan@gmail.com>
2021-11-21 19:28:10 +0200

    Update location

--- AMIGOS/website/our_classes/user_class.py
+++ AMIGOS/website/our_classes/user_class.py
@@ -42,7 +42,7 @@ class User():
         
 
         # append the new_user
-        with open(r'HTML-PYTHON\website\database\tabel_users.json', 'r') as file:
+        with open(r'AMIGOS\website\database\tabel_users.json', 'r') as file:
             current = json.load(file)
 
 
@@ -83,13 +83,13 @@ class User():
 
             current['0'] += 1
 
-            with open(r'HTML-PYTHON\website\database\tabel_users.json', 'w') as file:
+            with open(r'AMIGOS\website\database\tabel_users.json', 'w') as file:
                 json.dump(current, file, indent=4)
 
         return 'User has been added succesfully'
 
     def check_if_in_database(email_or_username, password):
-        with open(r'HTML-PYTHON\website\database\tabel_users.json', 'r') as file:
+        with open(r'AMIGOS\website\database\tabel_users.json', 'r') as file:
             current = json.load(file)
 
             for user_data in current.values():
@@ -102,40 +102,40 @@ class User():
 
     # change indent from None to 4 to pretty print
     def change_attr(user_id, my_dict):
-        with open(r'HTML-PYTHON\website\database\tabel_users.json', 'r') as file:
+        with open(r'AMIGOS\website\database\tabel_users.json', 'r') as file:
             current = json.load(file)
 
             for key, value in my_dict.items():
                 current[user_id][key] = value
 
-            with open(r'HTML-PYTHON\website\database\tabel_users.json', 'w') as file:
+            with open(r'AMIGOS\website\database\tabel_users.json', 'w') as file:
                 json.dump(current, file, indent=4)
 
     change_attr.__doc__ = "Attributes of a user are 'name', 'email', 'password', 'county', 'city', 'phone', 'avg_consumption', 'surface'"
 
     def delete_user(user_id):
-        with open(r'HTML-PYTHON\website\database\tabel_users.json', 'r') as file:
+        with open(r'AMIGOS\website\database\tabel_users.json', 'r') as file:
             current = json.load(file)
 
             del current[user_id]
 
-            with open(r'HTML-PYTHON\website\database\tabel_users.json', 'w') as file:
+            with open(r'AMIGOS\website\database\tabel_users.json', 'w') as file:
                 json.dump(current, file, indent=4)
 
     def delete_database():
-        with open(r'HTML-PYTHON\website\database\tabel_users.json', 'r') as file:
+        with open(r'AMIGOS\website\database\tabel_users.json', 'r') as file:
             current = {
                 "0" : 0,
             }
 
-            with open(r'HTML-PYTHON\website\database\tabel_users.json', 'w') as file:
+            with open(r'AMIGOS\website\database\tabel_users.json', 'w') as file:
                 json.dump(current, file, indent=4)
 
 
 
     def get_table_user():
         rez = {}
-        with open(r'HTML-PYTHON\website\database\tabel_users.json', 'r') as file:
+        with open(r'AMIGOS\website\database\tabel_users.json', 'r') as file:
             rez = json.load(file)
             
         return rez
@@ -144,9 +144,9 @@ class User():
 
 
     def pretty_print():
-        with open(r'HTML-PYTHON\website\database\tabel_users.json', 'r') as file:
+        with open(r'AMIGOS\website\database\tabel_users.json', 'r') as file:
             rez = json.load(file)
 
-            with open(r'HTML-PYTHON\website\database\tabel_users.json', 'w') as file:
+            with open(r'AMIGOS\website\database\tabel_users.json', 'w') as file:
                 json.dump(rez, file, indent=4)
 
