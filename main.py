f93333aa3eb680e2c8975cecc75df9e0fa8616ba
Lucky13forever <emanuellugojan@gmail.com>
2021-11-21 13:01:04 +0200

    Users

--- main.py
+++ main.py
@@ -2,4 +2,10 @@ from Implementare.user_functions import User
 
 # User.create_new_user('Emanuel', 'emanuel@gmail.com', '1234', 'Timis', 'Timisoara', '0724037007', 300, 20)
 
-User.delete_database()
+rez = User.create_new_user('Emanuel', 'emanuel@gmail.com', '1234', 'Timis', 'Timisoara', '0724037007', 300, 20)
+
+print(rez)
+
+rez = User.get_table_user()
+
+print(rez)
