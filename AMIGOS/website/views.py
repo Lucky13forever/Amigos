0bf60ef8ab2306e8b1d9b89cd4704d4ee1b019f6
Lucky13forever <emanuellugojan@gmail.com>
2021-11-23 15:40:54 +0200

    remove user_json from print

--- AMIGOS/website/views.py
+++ AMIGOS/website/views.py
@@ -6,7 +6,4 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    print(User.check_if_in_database('Emanuel', 'lugobugo'))
-    print(User.get_table_user())
-    print(User.check_if_in_database("Vlad", "vladvlad"))
     return render_template("home.html", table_user=User.get_table_user())
