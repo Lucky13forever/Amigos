c849ff833d3396b41f2cdf9ea401d23e409fa467
Lucky13forever <emanuellugojan@gmail.com>
2021-11-23 15:06:48 +0200

    Update models.py

--- AMIGOS/website/models.py
+++ AMIGOS/website/models.py
@@ -6,4 +6,7 @@ from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150))
     email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    
