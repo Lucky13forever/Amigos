177b43b37eb4f4b36bbf98d18237130bf403b67f
Lucky13forever <emanuellugojan@gmail.com>
2021-11-24 11:53:36 +0200

    .

--- AMIGOS/website/views.py
+++ AMIGOS/website/views.py
@@ -5,7 +5,6 @@ from flask_login import login_required, current_user
 views = Blueprint('views', __name__)
 
 @views.route('/')
-@login_required
 def home():
     
     return render_template("home.html", table_user=User.query.all(), user=current_user)
