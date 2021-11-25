0c8840e43c3f61af3c7cf506b041c6d2955cbc71
Lucky13forever <emanuellugojan@gmail.com>
2021-11-25 17:00:40 +0200

    .

--- AMIGOS/website/__init__.py
+++ AMIGOS/website/__init__.py
@@ -8,6 +8,7 @@ DB_NAME = "database.db"
 
 def create_app():
    app = Flask(__name__)
+   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
    app.config['SECRET_KEY'] = 'abcd1234'
    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
    db.init_app(app)
