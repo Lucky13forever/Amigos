dbeaffcddbcdfcb85ebe00bbebe35a48f24c9bab
Emil <tobyytb7@gmail.com>
2021-11-29 23:30:48 +0200

    antetul 2

--- AMIGOS/website/get_full_system.py
+++ AMIGOS/website/get_full_system.py
@@ -5,15 +5,8 @@ Cucu Raul
 Melnic Katalin
 Radu Apreotesei
 """
-from our_classes.panou_class import Panels
-from our_classes.accumulator_class import Accumulator
-from our_classes.Regulator_with_invertor_class import Regulator_with_invertor
-# from our_classes.get_panel_system import get_panel_system
-
-
-class get_system:
-    def get_full_system(self, user_budget: int, user_surface: float, user_location: str, panel_list: Panels,
-                        accumulator_list: Accumulator,
-                        regulators_with_invertors: Regulator_with_invertor,
-                        region_dict: dict):
-        print("This is just so i can see there is no error")
+def get_full_system(self, user_budget: int, user_surface: float, user_location: str, panel_list: Panels,
+                    accumulator_list: Accumulator,
+                    regulators_with_invertors: Regulator_with_invertor,
+                    region_dict: dict):
+    print("This is just so i can see there is no error")
