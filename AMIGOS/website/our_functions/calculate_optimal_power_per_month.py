e1fb3439cfb92cb3441bf9a27b95a53526c8eba6
vp999S <neagoevlad02@yahoo.com>
2021-12-01 17:08:25 +0200

    update

--- AMIGOS/website/our_functions/calculate_optimal_power_per_month.py
+++ AMIGOS/website/our_functions/calculate_optimal_power_per_month.py
@@ -3,3 +3,4 @@ def calculate_optimal_power_per_month(total_power: int):
     energy_for_a_day = total_power * 5
     energy_for_a_month = energy_for_a_day * 30 # energia pe o luna in W
     energy_for_a_month_kW = energy_for_a_month / 1000
+    return energy_for_a_month_kW
