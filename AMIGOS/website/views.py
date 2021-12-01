f1c2d6710b635eb9b04009d3e839f2f085998283
Lucky13forever <emanuellugojan@gmail.com>
2021-12-01 19:21:39 +0200

    adding test page

--- AMIGOS/website/views.py
+++ AMIGOS/website/views.py
@@ -28,3 +28,26 @@ def home():
     ramas_din_buget = result[3]
     return render_template("home.html", panouri=panouri, acumulatori=acumulatori, regulatori=regulatori, ramas=ramas_din_buget, ok = ok, user=current_user) #User.query.all()
 
+
+@views.route('/test')
+def test():
+    
+    # return render_template("home.html", user=current_user)
+
+    # prblema daca bugetul e prea mic, NonType Error
+    # result = get_full_system(2000, 10, 10, "Constanta", load_all_panels(), load_all_accumulators(), load_all_regulators(), load_region_dict())
+    # Suma ramasa din buget e -1727, deci as pune userul sa plateasca aproape dublu
+    # ToDo: daca nu reusim sa gasim niciun sistem, fix la return
+    result = get_full_system(10000, 10, 10, "Satu-Mare", load_all_panels(), load_all_accumulators(), load_all_regulators(), load_region_dict())
+    
+    print(result)
+
+    ok = 1
+    if result[0][0] == None:
+        ok = 0
+    
+    panouri = result[0]
+    acumulatori = result[1]
+    regulatori = result[2]
+    ramas_din_buget = result[3]
+    return render_template("home.html", panouri=panouri, acumulatori=acumulatori, regulatori=regulatori, ramas=ramas_din_buget, ok = ok, user=current_user) #User.query.all()
