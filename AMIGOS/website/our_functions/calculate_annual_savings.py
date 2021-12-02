5a2cee48ba2ef003f1cdbbf917e64d831b24a3c7
Lucky13forever <93001393+Lucky13forever@users.noreply.github.com>
2021-12-02 13:49:32 +0200

    Update calculate_annual_savings.py

--- AMIGOS/website/our_functions/calculate_annual_savings.py
+++ AMIGOS/website/our_functions/calculate_annual_savings.py
@@ -4,4 +4,5 @@ def calculate_annual_savings (price_per_kW: float, monthly_cost_without_system: 
     s=int(0)
     for i in range(0, 12):
         s += monthly_cost_without_system[i] - monthly_cost_with_system[i]
+    s=s*price_per_kW
     return s
