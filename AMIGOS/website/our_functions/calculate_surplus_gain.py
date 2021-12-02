cf6496ad5577fbb8663bf5dd098d679a66073b52
plushi69 <vlad.puscasu02@e-uvt.ro>
2021-12-02 11:40:02 +0200

    update

---
+++ AMIGOS/website/our_functions/calculate_surplus_gain.py
@@ -0,0 +1,9 @@
+def calculate_surplus_gain(price_per_kW :float, user_consumption :list, energy_production :list):
+    l=[]
+    for i in range(len(user_consumption)):
+        if user_consumption[i]>energy_production[i]:
+            l.append(0)
+        else:
+            val=(energy_production[i]-user_consumption[i]) * price_per_kW
+            l.append(val)
+    return l
