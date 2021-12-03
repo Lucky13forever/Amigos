d9684b09288f31d53fe33b6a6c6f971c640036ab
Lucky13forever <emanuellugojan@gmail.com>
2021-12-03 12:13:33 +0200

    apply_percent

---
+++ AMIGOS/website/our_functions/apply_percent.py
@@ -0,0 +1,3 @@
+def apply_percent(x,y):
+    y=(y*x)//100
+    return y
--- AMIGOS/website/our_functions/get_percent.py
+++ AMIGOS/website/our_functions/get_percent.py
@@ -1,7 +1,7 @@
 # Platon Aryana, Osman Raluca
+# cat la % este x din y
 def get_percent(x,y):
-    y=(y*x)//100
+    y = (x * 100) / y
     return y
-
 
 
