becb24c9e1f83b3598f8a1bcd3bac4af4b79ae30
Lucky13forever <emanuellugojan@gmail.com>
2021-12-08 00:36:52 +0200

    .

--- AMIGOS/website/views.py
+++ AMIGOS/website/views.py
@@ -127,7 +127,6 @@ def calculator():
 
         result = get_full_system(budget, user.roof_width, user.roof_length, user.county, get_all_panels, load_all_accumulators(), get_all_regulators, get_region_dict)
         
-        print(result[0][0].picture)
         return redirect(url_for("views.system", user=current_user, step=calculator_step, result=result, pictures=pictures))
 
 
